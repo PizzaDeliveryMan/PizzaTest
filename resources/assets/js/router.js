166b5b004c5de9913c05a99c9da157c4ee731e65
Niels Drost <nielsdrost7@gmail.com>
2018-05-04 14:24:17 +0200

    almost there : final Styles compiling, JS compiling

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -16,7 +16,7 @@ export default new VueRouter(
          * If switching back to default "hash" mode, don't forget to set the
          * build publicPath back to '' so Cordova builds work again.
          */
-        mode: 'history',
+        mode: '',
         routes: [
             {
                 path: '/example'
